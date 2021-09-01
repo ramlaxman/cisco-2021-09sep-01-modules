3373ee84318d81ac4882512393fdeee711e69aba
Reuven Lerner <reuven@lerner.co.il>
2021-09-01 10:38:48 +0300

    gitautosync update 62

--- mymod.py
+++ mymod.py
@@ -30,7 +30,28 @@ You normally want your function docstring to say:
     return f'Hello, {name}!'
 
 
+class Person:
+    '''A class that describes a person by name.'''
+
+    def __init__(self, first, last):
+        '''Creates a new Person object.
+
+        - Expects: Two strings, first and last
+        - Modifies: The instance
+        - Returns: The instance
+        '''
+        self.first = first
+        self.last = last
+
+    def greet(self):
+        '''Returns a string with a friendly greeting to the person.'''
+        return f'Hello, {self.first} {self.last}'
+
+
 # Meaning: If this program is run directly from the command line,
 # then print out "goodbye."  Otherwise, remain silent.
 if __name__ == '__main__':
     print(f'Goodbye from {__name__}!')
+
+    p = Person('Reuven', 'Lerner')
+    print(p.greet())
