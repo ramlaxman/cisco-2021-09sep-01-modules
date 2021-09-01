136495ded57e654baaabec8ae8bc531014e1dbda
Reuven Lerner <reuven@lerner.co.il>
2021-09-01 10:11:49 +0300

    gitautosync update 23

--- flycheck_menu.py
+++ flycheck_menu.py
@@ -1,2 +1,4 @@
-def menu(*args):   # this is pronounced "splat-args" in the Python world -- it means all positional arguments are assigned to t
+def menu(*args):   # this is pronounced "splat-args" in the Python world --
+                   # it means all positional arguments are assigned to the tuple args
+    
     
