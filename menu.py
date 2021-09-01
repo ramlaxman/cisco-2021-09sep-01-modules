8930247b6083345deb0ad353165c5d1a2e716f36
Reuven Lerner <reuven@lerner.co.il>
2021-09-01 10:13:30 +0300

    gitautosync update 28

--- menu.py
+++ menu.py
@@ -1,3 +1,13 @@
 def menu(*args):   # this is pronounced "splat-args" in the Python world --
     # it means all positional arguments are assigned to the tuple args
-    print(f'Choose one of these: {args}')
+
+    while True:
+
+        print(f'Choose one of these: {args}')
+
+        s = input('Enter your choice: ')
+
+        if s in args:
+            return s
+
+        print(f'{s} is not a valid input! Try again!')
