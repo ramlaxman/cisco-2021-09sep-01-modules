6b1345ae3e1e6056566684c7de8f42df452e536a
Reuven Lerner <reuven@lerner.co.il>
2021-09-01 10:16:03 +0300

    gitautosync update 34

--- menu.py
+++ menu.py
@@ -5,6 +5,8 @@ def menu(*args):   # this is pronounced "splat-args" in the Python world --
 
         print(f'Choose one of these: {args}')
 
+        options = '/'.join(sorted(args))
+
         # get the user's input, and remove whitespace from the sides
         s = input(f'Choose one: {options}').strip()
 
